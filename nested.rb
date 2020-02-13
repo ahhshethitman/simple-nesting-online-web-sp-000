6f603e1f59f28400d078e98e38fa0d5c72567397
Hunter Sides <hunter.s.1337@gmail.com>
2020-02-13 22:03:20 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,10 +81,15 @@ def adding_matz
           :known_for => "Unix",
           :languages => ["C"]
         }
+        :yukihiro_matsumoto => {
+          :known_for => "Ruby",
+          :languages => ["LISP", "C"]
+        }
+
      }
 
-    programmer_hash[:yukihiro_matsumoto] = "Value"
-   puts programmer_hash
+ puts programmer_hash
+
 end
 
 def changing_alan
